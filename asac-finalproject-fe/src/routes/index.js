b55ed4bdcd9b30daa6c23834fbd82d4d64d66d87
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2022-11-29 21:09:38 +0700

    add clinic and specialization page

--- asac-finalproject-fe/src/routes/index.js
+++ asac-finalproject-fe/src/routes/index.js
@@ -5,6 +5,8 @@ import Homepage from "../components/Homepage/homepage";
 import Doctor from "../components/Doctor/doctor";
 import DoctorDetail from "../components/Doctor/doctorDetail";
 import Booking from "../components/Booking/booking";
+import Clinic from "../components/Clinic/cliinic";
+import Specialization from "../components/Specialization/specialization";
 
 function RouteComponent() {
   return (
@@ -15,6 +17,8 @@ function RouteComponent() {
         <Route path="/doctor" element={<Doctor />}></Route>
         <Route path="/doctor/:id" element={<DoctorDetail />}></Route>
         <Route path="/booking/:id" element={<Booking />}></Route>
+        <Route path="/clinics" element={<Clinic />}></Route>
+        <Route path="/specializations" element={<Specialization />}></Route>
       </Routes>
     </>
   );
