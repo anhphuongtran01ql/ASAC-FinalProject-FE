f394a7cd69639394ec12ccddd5b5363d13e77124
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2022-12-09 20:55:11 +0700

    Get detail patients by doctors

---
+++ asac-finalproject-fe/src/components/Supporter/detail.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Button, PageHeader, Divider } from "antd";
+import { Link, useParams } from "react-router-dom";
+import { ProfileOutlined } from "@ant-design/icons";
+import "../Admin/user.css";
+import Loading from "../../components/General/Loading";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPatientById } from "../Services/Supporter/patientService";
+import { getPatientFeedback } from "../Services/Doctor/doctorService";
+
+export function PatientInfo() {
+  const { id } = useParams();
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["patient", id],
+    queryFn: () => fetchPatientById(id),
+  });
+
+  const {
+    data: comments,
+    isCommentLoading,
+    isCommentFetching,
+  } = useQuery({
+    queryKey: ["comments"],
+    queryFn: () => getPatientFeedback(data.doctorId),
+  });
+  console.log("comments", comments);
+  return (
+    <>
+      {isLoading || isCommentLoading || isFetching || isCommentFetching ? (
+        <Loading />
+      ) : (
+        <div className="info-container">
+          <PageHeader
+            title="Patient record"
+            onBack={() => window.history.back()}
+          >
+            <div className="specialization-info">
+              <h1 className="specialization-info-heading">{data.name}</h1>
+              <Divider className="divider-detail" orientation="left">
+                Personal Information
+              </Divider>
+              <div className="para-detail">
+                <p>
+                  <b>Gender: </b>
+                  {data.gender === 1 ? "Female" : "Male"}
+                </p>
+                <p>
+                  <b>Year of birth: </b>
+                  {data.year}
+                </p>
+                <p>
+                  <b>Phone Number: </b>
+                  {data.phone}
+                </p>
+                <p>
+                  <b>Email: </b>
+                  {data.email}
+                </p>
+                <p>
+                  <b>Address: </b>
+                  {data.address}
+                </p>
+              </div>
+
+              <Divider className="divider-detail" orientation="left">
+                Booking Information
+              </Divider>
+              <div className="para-detail">
+                <p>
+                  <b>Booked Doctor: </b>
+                  {data.doctorName}
+                </p>
+                <p>
+                  <b>Date Booking: </b>
+                  {new Date(data.dateBooking).toLocaleDateString("vi")}
+                </p>
+                <p>
+                  <b>Time Booking: </b>
+                  {data.timeBooking}
+                </p>
+                <p>
+                  <b>Description: </b>
+                  {data.description}
+                </p>
+                <p>
+                  <b>Feedback: </b>
+                  {comments ? (
+                    comments.map((item) => {
+                      return <p>{item.content}</p>;
+                    })
+                  ) : (
+                    <p>Have no comment!</p>
+                  )}
+                </p>
+              </div>
+            </div>
+          </PageHeader>
+        </div>
+      )}
+    </>
+  );
+}
+
+function PatientDetailInfo({ patientId }) {
+  return (
+    <>
+      <Link to={`/list-patients/${patientId}`}>
+        <Button
+          className="button-detail"
+          type="link"
+          icon={<ProfileOutlined />}
+        >
+          Detail
+        </Button>
+      </Link>
+    </>
+  );
+}
+
+export default PatientDetailInfo;
